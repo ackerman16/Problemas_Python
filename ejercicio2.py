e6e2719077e81a02807b370c20da07720bcf1fb3
Gumball <114919501+ackerman16@users.noreply.github.com>
2023-05-29 13:11:36 +0200

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+x = 3
+y = 3.3
+
+print("El tipo de x antes es", type(x))
+print("El tipo de y antes es", type(y))
+
+x = str(3)
+y = str(3.3)
+
+print("El tipo de x después es", type(x))
+print("El tipo de y después es", type(y))
